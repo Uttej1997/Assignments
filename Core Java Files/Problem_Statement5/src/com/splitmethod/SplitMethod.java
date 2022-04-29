18c9c210f12bb02d3d10e1fa71794c002d8454b5
Uttej <uttejawasthi210@gmail.com>
2022-04-29 10:44:41 +0530

    Core Java Assignments updated

--- Core Java Files/Problem_Statement5/src/com/splitmethod/SplitMethod.java
+++ Core Java Files/Problem_Statement5/src/com/splitmethod/SplitMethod.java
@@ -5,7 +5,7 @@ public class SplitMethod {
 public static void main(String[] args) {
 			
 		String txt= (" 23  +  45  -  (  343  /  12  ) ");
-		String[] w=txt.split"\\\s");
+		String[] w=txt.split("\\s");
 		
 		for(String w1:w){  
 			System.out.println(w1); 
